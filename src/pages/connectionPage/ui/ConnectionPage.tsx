1a5f3ae860340bc2fd1f96a3a9c21c094b092790
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-09-02 23:34:38 +0300

    buttons

--- src/pages/connectionPage/ui/ConnectionPage.tsx
+++ src/pages/connectionPage/ui/ConnectionPage.tsx
@@ -1,52 +1,21 @@
-
 import {TonConnectButton, useTonAddress} from "@tonconnect/ui-react";
-
-import styled from "styled-components";
-
-import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
-import {useTonConnect} from "../../../hooks/useTonConnect";
-import {FlexBoxCol, FlexBoxRow} from "../../../components/styled/styled";
-
-const StyledApp = styled.div`
-    background-color: #e8e8e8;
-    color: black;
-
-    @media (prefers-color-scheme: dark) {
-        background-color: #222;
-        color: white;
-    }
-    min-height: 100vh;
-    padding: 20px 20px;
-`;
-
-const AppContainer = styled.div`
-    max-width: 900px;
-    margin: 0 auto;
-`;
-
+// import {FlexBoxCol, FlexBoxRow} from "@/components/styled/styled";
+import css from './ConnectionPage.module.css'
+import {useEffect} from "react";
 
 export function ConnectionPage() {
-    const { network } = useTonConnect();
+    // const {network} = useTonConnect();
     const userFriendlyAddress = useTonAddress();
 
-    const sendData = () => {
-        window.Telegram.WebApp.sendData('test send data')
-    }
+    useEffect(() => {
+        // if (userFriendlyAddress) window.Telegram.WebApp.sendData(userFriendlyAddress)
+    }, [userFriendlyAddress]);
 
     return (
-        <StyledApp>
-            <AppContainer>
-                <FlexBoxCol>
-                    <FlexBoxRow>
-                        <TonConnectButton />
-                        <button onClick={sendData}>
-                            SEND DATA
-                        </button>
-                        {userFriendlyAddress && userFriendlyAddress}
-                    </FlexBoxRow>
-                </FlexBoxCol>
-            </AppContainer>
-        </StyledApp>
+        <div className={css.root}>
+            <h1>What to join crypto luxury?</h1>
+            <TonConnectButton/>
+        </div>
     );
 }
