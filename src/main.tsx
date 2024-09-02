1946bb906de558030cbcd35f141af422c76cb815
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-09-02 21:28:43 +0300

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {RouterProvider} from "react-router-dom";
 import "./index.css";
+import {appRouter} from "./app/appRouter";
 // this manifest is used temporarily for development purposes
 const manifestUrl = "https://Pomoika-Inc.github.io/average-channel-enjoyer-front/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+    defaultOptions: {queries: {refetchOnWindowFocus: false}},
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </TonConnectUIProvider>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={appRouter}/>
+        </QueryClientProvider>
+    </TonConnectUIProvider>
 );
