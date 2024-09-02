3cbe089eb7a72faf7e3e07c17bf6b61560768862
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-09-02 22:00:20 +0300

    imports config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]__[hash:8]',
+      localsConvention: null,
+    },
+  },
   base: "/average-channel-enjoyer-front/"
 });
