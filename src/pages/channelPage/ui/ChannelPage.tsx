bf1404bc8a6725385a9d6482bdf7726f63bd81f1
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-09-12 20:32:03 +0300

    channel page

--- src/pages/channelPage/ui/ChannelPage.tsx
+++ src/pages/channelPage/ui/ChannelPage.tsx
@@ -1,18 +1,44 @@
 import "@twa-dev/sdk";
 import css from './ChannelPage.module.css'
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "@/shared/ui/layouts/Layout";
 import {HeaderContent} from "./HeaderContent";
+import {ProductCard} from "@/entities/product";
 
 export function ChannelPage() {
 
     useEffect(() => {
     }, []);
 
+    const products = [
+        {
+            id: 3454353,
+            title: "Nya Poka",
+            price: 23,
+            description: "This is the best product you have even seen",
+            image: "/images/channelAva1.png",
+        },
+        {
+            id: 3454353,
+            title: "Nya Poka",
+            price: 23,
+            description: "This is the best product you have even seen",
+            image: "/images/channelAva1.png",
+        }
+    ]
+
     return (
         <>
             <Layout headerContent={<HeaderContent/>}/>
-            <div className={css.root}></div>
+            <div className="flex flex-col items-center">
+                <ul className="flex flex-col w-full p-3 gap-2">
+                    {products.map((product) => (
+                        <li>
+                            <ProductCard product={product} key={product.id}/>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
 }
