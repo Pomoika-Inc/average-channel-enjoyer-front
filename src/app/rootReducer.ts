16a9f60201073b32a7e0ba8242718483b51d7b84
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-10-13 19:03:46 +0300

    orders

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { userSlice } from '@/entities/user'
 import {productSlice} from "@/entities/product/model/productSlice";
 import {channelSlice} from "@/entities/channel/model/channelSlice";
+import {orderSlice} from "@/entities/order";
 
 export const rootReducer = combineReducers({
     [userSlice.name]: userSlice.reducer,
     [productSlice.name]: productSlice.reducer,
     [channelSlice.name]: channelSlice.reducer,
+    [orderSlice.name]: orderSlice.reducer,
 })
