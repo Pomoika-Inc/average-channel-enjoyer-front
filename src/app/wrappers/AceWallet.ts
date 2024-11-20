2c643ad5c7ac5ae0f39ce18f4e400e62d429d529
dalshenekuda <arseniyyyyyyy@gmail.com>
2024-11-20 09:58:15 +0200

    imports locks

---
+++ src/app/wrappers/AceWallet.ts
@@ -0,0 +1,1 @@
+export * from '../../../build/AceWallet/tact_AceWallet';
--- src/shared/ui/tabs/Tabs.tsx
+++ src/shared/ui/tabs/Tabs.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode} from 'react';
 import * as TabsRadix from '@radix-ui/react-tabs';
 import styles from './styles.module.scss';
+import combine from "classnames";
 
 export type Tab = {
     id: string,
@@ -13,10 +14,10 @@ interface TabsProps {
 }
 
 export const Tabs: React.FC<TabsProps> = ({tabs}) => (
-    <TabsRadix.Root className={styles.Root} defaultValue={tabs[0].id}>
-        <TabsRadix.List className={styles.List}>
+    <TabsRadix.Root className={combine(styles.Root)} defaultValue={tabs[0].id}>
+        <TabsRadix.List className={combine(styles.List, "bg-pink-500")}>
             {tabs.map(tab => (
-                <TabsRadix.Trigger className={styles.Trigger} value={tab.id} key={tab.id}>
+                <TabsRadix.Trigger className={combine(styles.Trigger, "bg-pink-500")} value={tab.id} key={tab.id}>
                     {tab.title}
                 </TabsRadix.Trigger>
             ))}
